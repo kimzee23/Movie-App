d1b3b217497c8a9d1be26df97dfa08406de45cb3
Gazar <kolajokimzee2020@gmailcom>
2025-07-16 10:44:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {RouterProvider} from "react-router";
+import router from "./router/routes";
+
+function App() {
+  return (
+
+  );
+}
+
+export default App;
