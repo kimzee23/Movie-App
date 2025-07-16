10ff98f70d69bbec701a18569a678ff931c25265
Gazar <kolajokimzee2020@gmailcom>
2025-07-16 10:56:20 +0100

    d

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {RouterProvider} from "react-router";
+import  RouterProvider from "react-router";
 import router from "./router/routes";
 
 function App() {
   return (
+      <>
+      <RouterProvider router={router}/>
+      </>
 
   );
 }
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,5 +2,14 @@ import {createBrowserRouter} from "react-router";
 
 const router =  createBrowserRouter([
 
+    {
+        path : "/login",
+        element:" <Login/>"
+    },
+    {
+     path : "/signup",
+     element:" <Signup/>"
+    }
+
 ])
 export default router;
